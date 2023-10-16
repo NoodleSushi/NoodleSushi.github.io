f0cd9c18bb11d83e99242e6583a9e131543492b9
NoodleSushi <ytmikesstuffs@gmail.com>
2023-10-16 10:24:45 +0800

    fix main page z-index

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,13 +5,13 @@ function Main() {
     <>
       <div className={styles.introContainer}>
         <div className={styles.introContent}>
+          <img className={styles.noodleImageShadow} src='/noodle-home.png' alt='noodlesushi'/>
+          <img className={styles.noodleImage} src='/noodle-home.png' alt='noodlesushi' />
           <div className={styles.introContentText}>
             <h1>Heya! I'm NoodleSushi!</h1>
             <h2>Will be working on my website soon.</h2>
             <h2>uwu</h2>
           </div>
-          <img className={styles.noodleImage} src='/noodle-home.png' alt='noodlesushi' />
-          <img className={styles.noodleImageShadow} src='/noodle-home.png' alt='noodlesushi'/>
         </div>
       </div>
     </>
