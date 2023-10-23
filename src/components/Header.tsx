f4f843143191c27b0f3266f31a454bb984938b7e
NoodleSushi <ytmikesstuffs@gmail.com>
2023-10-23 14:48:53 +0800

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,56 @@
 import styles from './Header.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { IconContext } from 'react-icons';
 import { MdMenu } from 'react-icons/md';
+import { HiMusicNote } from 'react-icons/hi';
+import { NavLink } from 'react-router-dom';
+
+const music = new Audio("/music.ogg");
+music.loop = true;
+music.volume = 0.5;
 
 function Header() {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [musicPlayback, setMusicPlayback] = useState(false);
+
+  useEffect(() => {
+    if (musicPlayback)
+      music.play();
+    else
+      music.pause();
+  }, [musicPlayback]);
 
   return (
     <header className={styles.navbar}>
       <div className={styles.navbarRow}>
         <h1 className={styles.logoText}>NoodleSushi</h1>
         <IconContext.Provider value={{ size: "48px", className: styles.icon }}>
-          <button className={styles.hamburger} onClick={() => setIsExpanded(!isExpanded)}>
-            <MdMenu />
-          </button>
+          <div className={styles.navbarRight}>
+            <button className={styles.resetButton} onClick={() => setMusicPlayback(!musicPlayback)}>
+              <HiMusicNote />
+            </button>
+            <button className={`${styles.resetButton} ${styles.hamburger}`} onClick={() => { setIsExpanded(!isExpanded); console.log('augh'); }}>
+              <MdMenu />
+            </button>
+            <nav className={`${styles.navMenu} ${isExpanded ? styles.expanded : ""}`}>
+              <ul>
+                <li>
+                  <NavLink to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <a target="_blank">About</a>
+                </li>
+                <li>
+                  <NavLink to="/contact">
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </IconContext.Provider>
-        <div className={styles.navMenu}>
-          <ul>
-            <li>
-              <a target="_blank">Home</a>
-            </li>
-            <li>
-              <a target="_blank">About</a>
-            </li>
-            <li>
-              <a target="_blank">Contact</a>
-            </li>
-          </ul>
-        </div>
       </div>
     </header>
   );
