81dfd31fc44191393080bbad858618ba3c062066
NoodleSushi <ytmikesstuffs@gmail.com>
2023-10-23 16:19:47 +0800

    make div for header space

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,38 +21,41 @@ function Header() {
   }, [musicPlayback]);
 
   return (
-    <header className={styles.navbar}>
-      <div className={styles.navbarRow}>
-        <h1 className={styles.logoText}>NoodleSushi</h1>
-        <IconContext.Provider value={{ size: "48px", className: styles.icon }}>
-          <div className={styles.navbarRight}>
-            <button className={styles.resetButton} onClick={() => setMusicPlayback(!musicPlayback)}>
-              <HiMusicNote />
-            </button>
-            <button className={`${styles.resetButton} ${styles.hamburger}`} onClick={() => { setIsExpanded(!isExpanded); console.log('augh'); }}>
-              <MdMenu />
-            </button>
-            <nav className={`${styles.navMenu} ${isExpanded ? styles.expanded : ""}`}>
-              <ul>
-                <li>
-                  <NavLink to="/">
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <a target="_blank">About</a>
-                </li>
-                <li>
-                  <NavLink to="/contact">
-                    Contact
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </IconContext.Provider>
-      </div>
-    </header>
+    <>
+      <header className={styles.navbar}>
+        <div className={styles.navbarRow}>
+          <h1 className={styles.logoText}>NoodleSushi</h1>
+          <IconContext.Provider value={{ size: "48px", className: styles.icon }}>
+            <div className={styles.navbarRight}>
+              <button className={styles.resetButton} onClick={() => setMusicPlayback(!musicPlayback)}>
+                <HiMusicNote />
+              </button>
+              <button className={`${styles.resetButton} ${styles.hamburger}`} onClick={() => { setIsExpanded(!isExpanded); console.log('augh'); }}>
+                <MdMenu />
+              </button>
+              <nav className={`${styles.navMenu} ${isExpanded ? styles.expanded : ""}`}>
+                <ul>
+                  <li>
+                    <NavLink to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <a target="_blank">About</a>
+                  </li>
+                  <li>
+                    <NavLink to="/contact">
+                      Contact
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </IconContext.Provider>
+        </div>
+      </header>
+      <div className={styles.navbarSpace} />
+    </>
   );
 }
 
