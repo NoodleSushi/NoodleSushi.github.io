5c4d876b8f087f1dbf3e45cbc1392513ea3d145e
NoodleSushi <ytmikesstuffs@gmail.com>
2024-02-27 21:30:31 +0800

    Add Email

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,7 @@ import styles from './Main.module.css';
 import SocialButton from '../components/SocialButton';
 
 function Main() {
+  const email: string = 'noodlesushinfo@gmail.com';
   const socials: [string, string, string][] = [
     ['/socials/github.svg', 'GitHub', 'https://github.com/NoodleSushi'],
     ['/socials/twitter.svg', 'Twitter', 'https://twitter.com/Noodle_Sushi'],
@@ -27,6 +28,12 @@ function Main() {
             </a>
           )}
         </div>
+      </div>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <svg width="44" height="32" viewBox="2 0 16 16">
+          <path fill='#ffffffde' d="M1.75 2h12.5c.966 0 1.75.784 1.75 1.75v8.5A1.75 1.75 0 0 1 14.25 14H1.75A1.75 1.75 0 0 1 0 12.25v-8.5C0 2.784.784 2 1.75 2ZM1.5 12.251c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V5.809L8.38 9.397a.75.75 0 0 1-.76 0L1.5 5.809v6.442Zm13-8.181v-.32a.25.25 0 0 0-.25-.25H1.75a.25.25 0 0 0-.25.25v.32L8 7.88Z" />
+        </svg>
+        <a className={styles.link} href={`mailto:${email}`}>{email}</a>
       </div>
     </>
   );
