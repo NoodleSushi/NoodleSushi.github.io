0cf283b53ad84926e2df77cdadb6e1c7551511de
Sine Sia <ytmikesstuffs@gmail.com>
2024-07-06 19:47:17 +0800

    Hide inactive socials

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,11 +7,11 @@ function Main() {
     ['/socials/github.svg', 'GitHub', 'https://github.com/NoodleSushi'],
     ['/socials/twitter.svg', 'Twitter', 'https://twitter.com/Noodle_Sushi'],
     ['/socials/youtube.svg', 'YouTube', 'https://www.youtube.com/@Noodle_Sushi'],
-    ['/socials/instagram.svg', 'Instagram', 'https://www.instagram.com/noodle.sushii'],
+    // ['/socials/instagram.svg', 'Instagram', 'https://www.instagram.com/noodle.sushii'],
     ['/socials/soundcloud.svg', 'SoundCloud', 'https://soundcloud.com/noodle_sushi'],
     ['/socials/itchdotio.svg', 'Itch.io', 'https://noodlesushi.itch.io'],
     ['/socials/newgrounds.svg', 'Newgrounds', 'https://noodlesushi.newgrounds.com'],
-    ['/socials/bsky.png', 'Bluesky', 'https://bsky.app/profile/noodlesushi.bsky.social'],
+    // ['/socials/bsky.png', 'Bluesky', 'https://bsky.app/profile/noodlesushi.bsky.social'],
   ];
 
   return (
